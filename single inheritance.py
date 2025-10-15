06be7187f4c73953f652ae18efa1dfd85a8b5c9d
rajbir-ji <sandhurajbir8085@gmail.com>
2025-10-15 14:29:36 +0530

    hgy

---
+++ single inheritance.py
@@ -0,0 +1,10 @@
+#single inheritance 
+class account:
+    def __init__(self,acc_no,balance):
+        self.acc_no = acc_no
+        self.balance = balance
+def show
+        print(f"Account No: {self.acc_no}, Balance: {self.balance}")
+   def__init__(self,acc_no,balance):
+class saving_account(account):
+    def deposit(self,amount)
