b7cc2ac388280336dc72b2109e1e838af8f23c7f
rajbir-ji <sandhurajbir8085@gmail.com>
2025-10-27 12:42:29 +0530

    f

---
+++ multipleinheritance.py
@@ -0,0 +1,18 @@
+class A:
+    def __init__ (self):
+        super().__init__()
+        print("A Constructor")
+class B(A):
+    def __init__ (self):
+        super().__init__()
+        print("B Constructor")
+class C(A):
+    def __init__ (self):
+        super().__init__()
+        print("C Constructor")
+class D(B,C):
+    def __init__ (self):
+        super().__init__()
+        print("D Constructor")
+
+obj=D()        
---
+++ visualize.py
@@ -0,0 +1,4 @@
+size =10 
+for i in range(size):
+    for j in range(size):
+     print('*', end=' ')
